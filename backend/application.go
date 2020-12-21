89104993f840ec652a2c4832833e4464f5b19b9e
soobinseo <soobin323025@gmail.com>
2020-12-21 23:50:15 +0900

    first commit

--- backend/application.go
+++ backend/application.go
@@ -1,5 +1,30 @@
 package backend
 
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"time"
+)
+
 func Application() {
 
+	gin.SetMode(gin.DebugMode)
+	r := gin.Default()
+	var PORT = 8000
+
+	// add handlers
+	r.Use()
+
+	server := &http.Server{
+		Handler:      r,
+		Addr:         fmt.Sprintf("0.0.0.0:%d", PORT),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ backend/main/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "boardProject/backend"
+
+func main() {
+
+	backend.Application()
+
+}
