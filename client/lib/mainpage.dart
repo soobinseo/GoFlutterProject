67da28c05b19d022cb47d4fa1b09e9d63670ac4d
soobinseo <soobin323025@gmail.com>
2021-01-02 17:41:11 +0900

    add flutter

---
+++ client/lib/mainpage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  _MainPage createState() => _MainPage();
+}
+
+class _MainPage extends State<MainPage> {
+
+  @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
